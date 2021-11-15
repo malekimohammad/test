60524781b7b8104a0cfa3dbd6a485dfa398a6290
mohammadmalekiii <94113041+mohammadmalekiii@users.noreply.github.com>
2021-11-15 11:59:15 +0330

    chamge MyList to UserList

--- components/UserList.jsx
+++ components/UserList.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const MyList = ({movie}) => {
+const MyList = ({movie:{poster_path}}) => {
     return (
         <>
             <li>
                 <div className="image-card">
-                    <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt="Image Movie" />
+                    <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} alt="Image Movie" />
                 </div>
                 <div className="content-card">
                     <h3 className="title-card">فیلم : {movie.title}</h3>
